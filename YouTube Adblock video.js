a890406a0571a3b8eda8c2e545fccec358c4b93c
antonlam <31198405+antonlam@users.noreply.github.com>
2025-09-21 14:11:40 +0800

    Update YouTube Adblock video.js

--- YouTube Adblock video.js
+++ YouTube Adblock video.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Youtube Adblock v4
+// @name         Youtube Adblock v2
 // @namespace    http://tampermonkey.net/
 // @version      2025-03-28
 // @description  Youtube Adblock v4
