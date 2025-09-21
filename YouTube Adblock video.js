5bb15a267e94bcd4a86f21e3e1f8ba6301e1fe69
antonlam1228 <antonlam1228@gmail.com>
2025-09-21 14:28:45 +0800

    Update YouTube Adblock video.js to version 4, refining ad removal functionality and improving variable management. Updated metadata including name and version.

--- YouTube Adblock video.js
+++ YouTube Adblock video.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Youtube Adblock v3
+// @name         Youtube-Adblock v4
 // @namespace    http://tampermonkey.net/
-// @version      2025-05-09
-// @description  Youtube Adblock v3
+// @version      2025-09-21
+// @description  Youtube Adblock v4
 // @author       Anton
 // @match        *://*.youtube.com/*
 // @exclude      *://accounts.youtube.com/*
@@ -13,88 +13,76 @@
 // @license MIT
 // ==/UserScript==
 
-var url = "";
-var video;
-var videoTime = 0;
-var videoInfo = {
-    "time" : 0,
-    "status" : '',
-    "speed" : 1.0,
-    "quality" : '',
-    "mode" : '',
-};
-
-const cssArrObject = [
-        `#masthead-ad`,
-        `ytd-rich-item-renderer.style-scope.ytd-rich-grid-row #content:has(.ytd-display-ad-renderer)`,
-        `.video-ads.ytp-ad-module`,
-        `tp-yt-paper-dialog:has(yt-mealbar-promo-renderer)`,
-        `ytd-engagement-panel-section-list-renderer[target-id="engagement-panel-ads"]`,
-        `#related #player-ads`,
-        `#related ytd-ad-slot-renderer`,
-        `ytd-ad-slot-renderer`,
-        `yt-mealbar-promo-renderer`,
-        `ytd-popup-container:has(a[href="/premium"])`,
-        `ad-slot-renderer`,
-        `ytm-companion-ad-renderer`,
-        `#related #-ad-`,
-    ];
+var cssArrObject = [
+    `#masthead-ad`,
+    `ytd-rich-item-renderer.style-scope.ytd-rich-grid-row #content:has(.ytd-display-ad-renderer)`,
+    `.video-ads.ytp-ad-module`,
+    `tp-yt-paper-dialog:has(yt-mealbar-promo-renderer)`,
+    `ytd-engagement-panel-section-list-renderer[target-id="engagement-panel-ads"]`,
+    `#related #player-ads`,
+    `#related ytd-ad-slot-renderer`,
+    `ytd-ad-slot-renderer`,
+    `yt-mealbar-promo-renderer`,
+    `ytd-popup-container:has(a[href="/premium"])`,
+    `ad-slot-renderer`,
+    `ytm-companion-ad-renderer`,
+    `#related #-ad-`,
+];
 
 (function() {
-    'use strict';
-    window.dev=false;
+let video;
+let videoTime;
+let mainVideoTime = 0;
+window.dev=false;
 
-    function removeNonVideoAds(arry) {
-        arry.forEach((selector, index) => {
-            arry[index] = `${selector}{display:none!important}`;
-        });
+function removeNonVideoAds(arry) {
+    arry.forEach((selector, index) => {
+        arry[index] = `${selector}{display:none!important}`;
+    });
 
-        const premiumContainers = [...document.querySelectorAll(`ytd-popup-container`)];
-        const matchingContainers = premiumContainers.filter(container =>
-            container.querySelector(`a[href="/premium"]`));
+    const premiumContainers = [...document.querySelectorAll(`ytd-popup-container`)];
+    const matchingContainers = premiumContainers.filter(container =>
+        container.querySelector(`a[href="/premium"]`));
 
-        if (matchingContainers.length > 0) {
-            matchingContainers.forEach(container => container.remove());
-        }
-
-        const backdrops = document.querySelectorAll(`tp-yt-iron-overlay-backdrop`);
-        const targetBackdrop = Array.from(backdrops).find(
-            (backdrop) => backdrop.style.zIndex === `2201`
-        );
-
-        if (targetBackdrop) {
-            targetBackdrop.className = ``;
-            targetBackdrop.removeAttribute(`opened`);
-        }
-        let style = document.createElement(`style`);
-        (document.head || document.body).appendChild(style);
-        style.appendChild(document.createTextNode(arry.join(` `)));
+    if (matchingContainers.length > 0) {
+        matchingContainers.forEach(container => container.remove());
     }
 
-    function skipAd(adsVideo) {
-        const adIndicator = document.querySelector(
-            '.ytp-ad-skip-button, .ytp-skip-ad-button, .ytp-ad-skip-button-modern, ' +
-            '.video-ads.ytp-ad-module .ytp-ad-player-overlay, .ytp-ad-button-icon'
-        );
+    const backdrops = document.querySelectorAll(`tp-yt-iron-overlay-backdrop`);
+    const targetBackdrop = Array.from(backdrops).find(
+        (backdrop) => backdrop.style.zIndex === `2201`
+    );
 
-        if (adIndicator && !window.location.href.includes('https://m.youtube.com/')) {
-            adsVideo.muted = true;
-            adsVideo.currentTime = adsVideo.duration - 0.1;
-            adsVideo.play();
-        }
+    if (targetBackdrop) {
+        targetBackdrop.className = ``;
+        targetBackdrop.removeAttribute(`opened`);
     }
+    let style = document.createElement(`style`);
+    (document.head || document.body).appendChild(style);
+    style.appendChild(document.createTextNode(arry.join(` `)));
+}
 
-    const mainFunc = setInterval(() => {
-        video = document.getElementById("movie_player") || document.getElementsByClassName("html5-video-player")[0];
-        if (document.readyState !== 'loading') {
-            window.addEventListener('beforeunload', () => {
-                window.localStorage.setItem('lastUrl', window.location.href);
-            }, { once: true });
-            let adsVideo = document.querySelector('.ad-showing video');
-            removeNonVideoAds(cssArrObject);
-            //url = window.location.href;
-            skipAd(adsVideo);
-        }
-    }, 100);
+function skipAd(video) {
+    const adIndicator = document.querySelector(
+        '.ytp-ad-skip-button, .ytp-skip-ad-button, .ytp-ad-skip-button-modern, ' +
+        '.video-ads.ytp-ad-module .ytp-ad-player-overlay, .ytp-ad-button-icon'
+    );
+
+    if (adIndicator && !window.location.href.includes('https://m.youtube.com/')) {
+        video.muted = true;
+        video.currentTime = video.duration;
+    }
+}
+
+setInterval(() => {
+    if (document.readyState !== 'loading') {
+        window.addEventListener('beforeunload', () => {
+            window.localStorage.setItem('lastUrl', window.location.href);
+        }, { once: true });
+        removeNonVideoAds(cssArrObject);
+        let video = document.querySelector('.ad-showing video');
+        skipAd(video);
+    }
+}, 500);
 
 })();
